db2356f118041b4053f36c3c227f1f7a9fa3a0cb
CamiGil <camilabgil@hotmail.com>
2020-04-23 00:18:26 -0300

    first commit

---
+++ src/main/java/com/agileengine/AgileEngineTestTask/AgileEngineTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.agileengine.AgileEngineTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileEngineTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileEngineTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agileengine/AgileEngineTestTask/AgileEngineTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agileengine.AgileEngineTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgileEngineTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
